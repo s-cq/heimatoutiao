9fafe00594a3379714643721a2ca2233de6a3811
s-cq <534581923@qq.com>
2019-09-18 11:04:23 +0800

    zz

--- src/router.js
+++ src/router.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home/'
+import Login from './views/login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      redirect: '/home'
+    },
+    {
+      path: '/login',
+
+      component: Login
+    },
+    {
+      path: '/home',
+
       component: Home
     }
+
     // {
     //   path: '/about',
     //   name: 'about',
